82642389b816ef7a4c85749c125e206d595957ed
Yuancheng Zhang <old.far.hot@gmail.com>
2015-04-01 15:53:45 -0500

    add sas.command

---
+++ cmd_linux.sh
@@ -0,0 +1,11 @@
+################
+#### Linux #####
+################
+
+# To confirm that your A records have been set correctly, use the "dig" command;
+dig example.com +nostats +nocomments +nocmd
+
+################
+##### Vim ######
+################
+
